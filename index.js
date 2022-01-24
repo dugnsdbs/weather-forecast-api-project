2c00bfa0ecb85fe57d91bfa0a9ee3602c744d78e
Aaron Kaplan <aaronokaplan@gmail.com>
2022-01-24 10:23:13 -0500

    Updated index.js with pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+// IDEA : 
+// Look up and comment on weather forecast by city
+
+// HTML
+
+// JAVASCRIPT FUNCTIONALITY :
+// DOMContentLoaded : display forecast for a certain location on the current date
+// Click : ‘like’ or ‘dislike’ weather
+// Submit : enter date + location for forecast lookup and comment on the forecast 
+
+// CSS
+
+
+
