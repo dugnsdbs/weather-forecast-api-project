a3586e06b7772fa7168de8ae36d90d8792aa1574
Aaron Kaplan <aaronokaplan@gmail.com>
2022-01-25 15:20:56 -0500

    Updated index.js with new functionality +  index.html and  style.css with revised class names, etc

--- index.js
+++ index.js
@@ -1,101 +1,132 @@
-const baseURL = 'https://www.metaweather.com/api/location';
-let city = 'new%20york';
+const baseURL = 'https://www.metaweather.com/api/location/';
+let locationInput = "new%20york"
+
+function setFormattedLocation (str) {
+    locationInput = str.replace(' ', '%20') 
+}
+
+// form create addEent
+const form = document.querySelector(".area-check");
+const input = document.querySelector("input.search")
+// addEvent
+form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        setFormattedLocation(input.value);
+        lookUpByLocation();
+        form.reset();
+})
+
+
+const date = new Date();
+const today = date.getDay();
+const yyyyYear = date.getFullYear();
+const mmMonth = date.getMonth() > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
+let ddDay = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+const week = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+};
+
+const month = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+};
+
+const placeAndTime = document.querySelector('.place-and-time');
+const todaysForecast = document.querySelector('#todays-forecast');
+
+const weekdays = document.querySelectorAll('span.dayOfWeek');
+const fiveDayForecast = document.querySelectorAll('.forecast-card');
+
+let nextDay = 0;
+for (day of weekdays){
+    day.textContent = week[(today + nextDay) % 7]
+    nextDay++;
+}
+
+
+//Initially populate with weather icons, replace with weather on search.
 
 document.addEventListener('DOMContentLoaded', () => {
-    getLocation();
-    locationSearch ()
-    locationSearchs ()
+    lookUpByLocation();
+});
 
-})
-function locationSearchs (){
-    fetch(`${baseURL}/search/?query=san`)
-    .then(response => response.json())
-    .then(citys => {
-        console.log(citys)
-    })
-}
-
-
-
-
-
-// fetch One City Info
-function locationSearch (){
-    fetch(`${baseURL}/search/?query=${city}`)
-    .then(response => response.json())
-    .then(city => {
-        console.log(city)
-        updateCityList (city)    
-    })
-}
-
-// forEach city Info
-function updateCityList (city){
-    city.forEach(cityInfo => currentLocation(cityInfo))
-}
-// search submit
-// need to get info from different api url
-function currentLocation(cityInfo){
-// city name variable
-    let currentLocation = cityInfo.title;
-// purple section where the name of city will appear
-    const cityDisplaySection = document.querySelector("body > div.search-area > h2");
-// form create addEent
-    const form = document.querySelector("body > nav > div > form");
-// addEvent
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault()
-        let input = e.target["city-name-zipcode"].value;
-        if(input == currentLocation){
-            cityDisplaySection.innerText = "";
-            cityDisplaySection.innerText = `${input}`
-        }else{
-            cityDisplaySection.innerText ="wrong city";
-        }
-    })
-}
-
-
-
-
-// five days info from API
-function getLocation (){
-    fetch(`${baseURL}/44418/`)
+function lookUpByLocation () {
+    fetch(`${baseURL}search/?query=${locationInput}`)
     .then(res => res.json())
     .then(data => {
-        // console.log(data.consolidated_weather)
-        let weatherInfo = data.consolidated_weather;
-        updateList(weatherInfo)
-        todayWeather (weatherInfo) // use only current date data
-       
-        // console.log(weatherInfo)
-    });
+        getWeatherData(data);
+        console.log(data)
+    })
+    .catch(error => {
+        alert("Sorry, we don't have data on that location. Please try another!");
+        form.reset();
+})
 }
-// obj data converted into Array
-function updateList(weatherInfo){
-    weatherInfo.forEach(dailyWeather => {
-        // console.log(dailyWeather) // to see all info
-        // console.log(`Weather Status: ${dailyWeather.weather_state_name}`) // to see object value belongs to specific object key
-        fiveDays(dailyWeather)
-        // todayWeather (dailyWeather)
+
+function getWeatherData (inData) {
+    fetch(`${baseURL}${inData[0].woeid}`)
+    .then(res => res.json())
+    .then(outData => {
+        console.log(outData);
+        displayCurrentForecast(outData);
+        displayFiveDayForecast(outData);
+        // displayFiveDayForecast
+        // placeAndTime.textContent = 
+        // `Forecast for ${outData.parent.title}, ${week[today]}, ${month[date.getMonth() + 1]} ${ddDay}, ${yyyyYear}:`
+        console.log(outData.consolidated_weather[0])
+        // todaysForecast.textContent = `${}`
     })
 }
 
+function displayFiveDayForecast (weatherData) {
+    let selectDay = 0;
+    for (day of fiveDayForecast) {
+        day.textContent = week[(today + selectDay) % 7]; 
+        const daysWeather = weatherData.consolidated_weather[selectDay];
+        const highTemp = document.createElement('li');
+        const lowTemp = document.createElement('li');
+        const conditions = document.createElement('li');
+        highTemp.textContent =  `HIGH: ${celsiusToF(Math.trunc(daysWeather.max_temp))}° F`;
+        lowTemp.textContent = `LOW: ${celsiusToF(Math.trunc(daysWeather.min_temp))}° F,` 
+        conditions.textContent = `CONDITIONS: ${sentenceCase(daysWeather.weather_state_name)}.`;
+        day.appendChild(highTemp);
+        day.appendChild(lowTemp);
+        day.appendChild(conditions);
+        selectDay++;
+    }
+}
 
+function displayCurrentForecast (weatherData) {
+    const currentForecast = weatherData.consolidated_weather[0]
+    placeAndTime.textContent = `Forecast for ${weatherData.title} today (${week[today]}, 
+                                ${month[date.getMonth() + 1]} ${ddDay}, ${yyyyYear}):`
+    todaysForecast.textContent = `HIGH: ${celsiusToF(currentForecast.max_temp)}° F
+                                LOW: ${celsiusToF(currentForecast.min_temp)}° F, 
+                                CONDITIONS: ${sentenceCase(currentForecast.weather_state_name)}.`;
+}
 
+function sentenceCase (string) {
+    return string[0].concat(string.slice(1).toLowerCase())
+}
 
-//display 5 days weather into HTML
-// DAILY forcaseSection
-function fiveDays(dailyWeather){
-    const dateOne = document.getElementById('date_one');
-    dateOne.innerText = "";
-    dateOne.innerText = Date();
-
-    const dateTwo= document.getElementById('date_two');
-    dateTwo.innerText ="";
-    dateTwo.innerText = Date()+1
-
-
+function celsiusToF (temp) {
+    return (temp * 9/5) + 32; 
 }
 
 // use current date data 
