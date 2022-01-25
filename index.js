fb55960b54a9757f77fd154b5e3215f70f32f457
Aaron Kaplan <aaronokaplan@gmail.com>
2022-01-25 09:52:26 -0500

    initiated index.js changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const baseURL = 'https://www.metaweather.com/api/location';
+let city = 'new%20york';
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`${baseURL}/search/?query=${city}`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+})
+
+
+
+
 
 // IDEA : 
 // Look up and comment on weather forecast by city
