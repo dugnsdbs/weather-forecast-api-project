391f1b946e096371d769a113a2174407865dea70
yeohoon yoon <yhoon.yoon@gmail.com>
2022-01-25 11:02:27 -0500

    updated

--- index.js
+++ index.js
@@ -2,23 +2,42 @@ const baseURL = 'https://www.metaweather.com/api/location';
 let city = 'new%20york';
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(`${baseURL}/search/?query=${city}`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+    getLocation();
+
+
 })
 
+// five days info from API
+function getLocation (){
+    fetch(`${baseURL}/44418/`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data.consolidated_weather)
+        let weatherInfo = data.consolidated_weather;
+        updateList(weatherInfo)
+    });
+}
+// obj data converted into Array
+function updateList(weatherInfo){
+    weatherInfo.forEach(dailyWeather => {
+        console.log(dailyWeather) // to see all info
+        console.log(`Weather Status: ${dailyWeather.weather_state_name}`) // to see object value belongs to specific object key
+        fiveDays(dailyWeather)})
+}
+
+//display 5 days weather into HTML
+// DAILY forcaseSection
+function fiveDays(dailyWeather){
+    const dateOne = document.getElementById('date_one');
+    dateOne.innerText = "";
+    dateOne.innerText = Date();
+
+    const dateTwo= document.getElementById('date_two');
+    dateTwo.innerText ="";
+    dateTwo.innerText = Date()+1
+
+
+}
 
 
 
-
-// IDEA : 
-// Look up and comment on weather forecast by city
-
-// HTML
-
-// JAVASCRIPT FUNCTIONALITY :
-// DOMContentLoaded : display forecast for a certain location on the current date
-// Click : ‘like’ or ‘dislike’ weather
-// Submit : enter date + location for forecast lookup and comment on the forecast 
-
-// CSS
