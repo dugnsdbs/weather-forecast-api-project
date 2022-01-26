cb8a56a22ffa1c513080d06c4427e205e38b1638
yeohoon yoon <yhoon.yoon@gmail.com>
2022-01-26 17:42:13 -0500

    updated html

--- index.js
+++ index.js
@@ -144,38 +144,6 @@ function sentenceCase (string) {
 function celsiusToF (temp) {
     return (temp * 9/5) + 32; 
 }
-// use current date data 
-// apply its data into the last part
-function todayWeather (weatherInfo) {
-    let todayWeather = weatherInfo[0];
-// add % sign at the end
-    let humidity = todayWeather.humidity;
-// need to change parseInt and fahrenheit
-    let maxTemp = Math.trunc(todayWeather.max_temp);
-// need to change parseInt and fahrenheit
-    let minTemp = Math.trunc(todayWeather.min_temp);
-// need to change parseInt
-    let visibility = Math.trunc(todayWeather.visibility);
-// need to change parseInt and miles 
-    let winSpeed = Math.trunc(todayWeather.wind_speed);  
-//temperature
-    const temperatureChart = document.querySelector("#temperate");
-        temperatureChart.innerText = "";
-        temperatureChart.innerText = `High : ${maxTemp} & Low: ${minTemp}`
-// humidity
-    const humidityChart = document.querySelector("#percentage");
-        humidityChart.innerText = "";
-        humidityChart.innerText = `Humidity : ${humidity} %`;
-// windSpeed
-    const winSpeedChart = document.querySelector("#kilometer");
-        winSpeedChart.innerText = "";
-        winSpeedChart.innerText = `Wind : ${winSpeed} Km/h`;
-// visibility
-    const visibilityChart = document.querySelector("#visibility");
-        visibilityChart.innerText = "";
-        visibilityChart.innerText = `Visibility : ${visibility}`;
-
-}
 
 const donationForm = document.querySelector("form.donation");
 donationForm.addEventListener("submit", (e) =>{
